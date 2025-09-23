1645cf292c57a6313a34594add40df74df53dff6
gy-gfigueras <willstar111yt@gmail.com>
2025-09-23 14:40:47 +0200

    refactor: update Jenkins pipeline to enhance workspace preparation and improve test execution steps

--- __mocks__/next-image-mock.js
+++ __mocks__/next-image-mock.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 module.exports = ({ src, alt, width, height, ...rest }) => {
   // Return a plain img element string for simple rendering in tests
   return require('react').createElement('img', {
